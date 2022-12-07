029c31d37c45c8178abb1a10e69d32325a2a7f4b
Honkato <77444910+Honkato@users.noreply.github.com>
2022-12-07 11:23:38 -0300

    Update Fibonnaci.java

--- Fibonnaci/src/bosch/Fibonnaci.java
+++ Fibonnaci/src/bosch/Fibonnaci.java
@@ -34,7 +34,7 @@ public class Fibonnaci {
 
     }
     public String[] thanos (){
-        String[] nomes = {"Batista", "Tejero", "Alex", "Gustavo", "Michael", "Emilly", "Naelly", "Luiza", "Grazielly", "adhayne", "Ghuilherme", "Beatriz", "Tayssa", "Macharete", "Raissa", "Radija", "Capovila", "Livia"};
+        String[] nomes = {"Batista", "Tejero", "Alex", "Gustavo", "Michael", "Emilly", "Naelly", "Luiza", "Grazielly", "Adhayne", "Guilherme", "Beatriz", "Tayssa", "Macharete", "Raissa", "Radija", "Capovila", "Livia"};
         String[] nomesAux;
         String nome;
         System.out.println(Arrays.toString(nomes));
@@ -47,16 +47,48 @@ public class Fibonnaci {
                     indexNome = i;
                 }
             }
-            System.out.println(indexNome);
+//            System.out.println(indexNome);
             for (int i = 0; i < nomes.length; i++) {
                 if (i != indexNome)
                     nomesAux = new String[]{Arrays.toString(nomesAux), nomes[i]};
             }
-            System.out.println(Arrays.toString(nomesAux));
+//            System.out.println(Arrays.toString(nomesAux));
             nome = Arrays.toString(nomesAux);
             nomes = nome.replace("[], ", "").replace("[", "").replace("]", "").replace(",", "").split(" ");
         }
+        System.out.println("*Click!");
+        return nomes;
+    }
 
+    public String[] thanos (String[] name){
+        String[] nomes = name;
+        String[] nomesAux;
+        String nome;
+        System.out.println(Arrays.toString(nomes));
+        int indexNome = 0;
+        int metade = nomes.length / 2;
+        if (nomes.length % 2 != 0){
+            metade +=1;
+        }
+
+        for (int x = 0; x < metade; x++) {
+            nomesAux = new String[]{};
+            nome = nomes[r.nextInt(nomes.length)];
+            for (int i = 0; i < nomes.length; i++) {
+                if (nomes[i].equals(nome)) {
+                    indexNome = i;
+                }
+            }
+//            System.out.println(indexNome);
+            for (int i = 0; i < nomes.length; i++) {
+                if (i != indexNome)
+                    nomesAux = new String[]{Arrays.toString(nomesAux), nomes[i]};
+            }
+//            System.out.println(Arrays.toString(nomesAux));
+            nome = Arrays.toString(nomesAux);
+            nomes = nome.replace("[], ", "").replace("[", "").replace("]", "").replace(",", "").split(" ");
+        }
+        System.out.println("*Click!");
         return nomes;
     }
 
